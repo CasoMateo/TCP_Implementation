d2d77df2c4f6bf1096ec3fc5397098f86ded9e12
CasoMateo <casomateo8@gmail.com>
2023-03-22 18:19:32 -0500

    tcp setup

--- tcp_protocol/src/main.rs
+++ tcp_protocol/src/main.rs
@@ -1,10 +1,33 @@
 use std::io;
-
+use std::net::Ipv4Addr;
 
 fn main() -> io::Result<()> {
     example_read_package_loop_parsing_flags_protocol_ignore_no_ipv4()
 }
 
+pub struct Ipv4Slice {
+    protocol: u16
+    source_addr: String
+    destination_addr: String
+    source_port: u16 
+    destination_port: u16
+}
+
+fn parseSlice(buffer: &[u8]) -> Packet {
+    let protocol = u16::from_be_bytes([buffer[0], buffer[1]]);
+    let source_address = format!("{}.{}.{}.{}", buffer[12], buffer[13], buffer[14], buffer[15]);
+    let destination_address = format!("{}.{}.{}.{}", buffer[16], buffer[17], buffer[18], buffer[19]);
+    let source_port = Some(u16::from_be_bytes(&buffer[0..2].try_into().unwrap()))
+    let destination_port = Some(u16::from_be_bytes(&buffer[2..4].try_into().unwrap()))
+
+    return Ipv4Slice {
+        protocol,
+        source_address,
+        destination_address,
+        source_port,
+        destination_port
+    }
+}
 
 fn example_read_single_package() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
@@ -72,12 +95,12 @@ fn example_read_package_loop_using_etherparser_flags_protocol_ignore_no_ipv4() -
             continue;
         }
 
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+        match parseSlice(&buf[4..nbytes]) {
             Ok(p) => {
                 // (src_ip, src_port, dst_ip, dst_port) -> quad
-                let src = p.source_addr();
-                let dst = p.destination_addr();
-                let proto = p.protocol();
+                let src = p.source_addr;
+                let dst = p.destination_addr;
+                let proto = p.protocol;
                 eprintln!(
                     "{} -> {} {} bytes of protocol: {:x}", src, dst, p.payload_len(), proto);
             },
